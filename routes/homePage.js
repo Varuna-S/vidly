6c9701cc6270844a8a19c675848fee6197b35439
Varuna-S <sathyavaruna@gmail.com>
2021-04-15 19:18:01 +0530

    updated homePage.js message

--- routes/homePage.js
+++ routes/homePage.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (request, response) =>{
-    response.send('Hello World');
+    response.send('Hello World - Welcome to Vidly');
 });
 
 module.exports = router;
